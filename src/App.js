70bf02522e00552234965dcb3bbe51558524dd69
serrau <aucher.serr@gmail.com>
2018-03-20 22:04:16 -0400

    basic framework, importing json

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import React from "react";
+import React, { Component } from 'react';
 import ReactDOM from "react-dom";
+import * as d3 from 'd3';
+import _ from 'lodash';
 
 import './css/style.css'; // Import CSS -> ADDED IN THIS STEP
+import geoData from '../nyc-data-playground/data/json/mn_features';
 
+var width = 900;
+var height = 1200;
+var margin = {left: 100, right: 20, top: 100, bottom: 20};
 
+class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            buildings : [],
+        };
+    }
 
-const App = () => {
-    return (
-        <div>
-        <p>React here!</p>
-    </div>
-);
-};
+    componentWillMount(){
+        // process data
+        console.log(geoData);
+    }
+
+    render() {
+        var props = {
+            width, margin
+        };
+
+        return (
+            <div className='App'>
+                <h2>
+                    Hi! This is my first app!
+                </h2>
+            </div>
+        );
+    }
+}
+
 export default App;
+
 ReactDOM.render(<App />, document.getElementById("app"));
