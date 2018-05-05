dcba6bc01207796b8d642b61f4ecf9b77d168eb8
serrau <aucher.serr@gmail.com>
2018-05-04 23:51:15 -0400

    get neighborhood mapping

--- nyc-data-playground/getNeighborhoodMapping.js
+++ nyc-data-playground/getNeighborhoodMapping.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const _ = require('lodash')
+var polygonCenter = require('geojson-polygon-center')
 
 path = './data/jsons/neighborhoods.json';
 
@@ -8,12 +9,19 @@ fs.readFile(path, (err, data) =>{
   else {
     let json = JSON.parse(data);
 
+    // console.log(json[0].geometry, polygonCenter(json[0].geometry));
+
     let map = _.chain(json)
         .uniqBy('properties.NTACode')
-      .map( d => {return {'NTACode': d.properties.NTACode, 'NTAName': d.properties.NTAName}; } )
+      .map( d => {
+        return {'NTACode': d.properties.NTACode,
+          'NTAName': d.properties.NTAName,
+          'center': polygonCenter(d.geometry).coordinates
+        };
+      } )
       .value();
-    //
-    console.log(map);
+
+    // console.log(map)
     fs.writeFileSync('./assets/neighborhoodMapping.json', JSON.stringify(map, null, 2))
   }
 })
