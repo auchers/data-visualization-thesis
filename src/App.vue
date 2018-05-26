2be3314c3f9da3985547d7c9c0940df4178ffd27
serrau <aucher.serr@gmail.com>
2018-05-26 15:10:32 +0300

    responsiveness tweaks and build for deployment

--- src/App.vue
+++ src/App.vue
@@ -105,33 +105,86 @@ export default {
     font-size: larger;
   }
 
-  @media only screen and (max-width: 400px) {
-    .container{
-      grid-template-columns: 100%;
+  @media only screen and (max-width: 415px) {
+    body > .container {
+      grid-template-columns: auto;
       grid-template-rows: 50vh 50vh;
       position: fixed;
       top: 0;
+      left: 0;
     }
 
-    .right-panel{
+    body .nav-header{
+      top: 55vh;
+      left: 1em;
+    }
+
+    .subtitle {
+      font-size: .5em;
+    }
+
+    .right-panel {
       overflow: scroll;
       -webkit-overflow-scrolling: touch;
       background-color: white;
       z-index: 1;
-      position:relative;
-      top:50%;
+      position: relative;
+      top: 50%;
+      border-top: rgba(131, 131, 131, 0.2) 1em solid;
     }
 
-    #mapbox{
+    body #mapbox {
       height: 50vh;
     }
 
-    .analysis{
+    .analysis {
       min-height: unset;
     }
 
-    div{
+    div {
       /*background-color: white;*/
     }
+
+    body .footer-container{
+      top: 1em;
+      bottom: unset;
+
+    }
+
+  }
+  @media only screen and (max-width: 780px) {
+    h1, h2, header{
+      font-size: 2em;
+      line-height: 1em;
+    }
+
+    p, table, span{
+      line-height: 1.1rem;
+      font-size: .9rem;
+    }
+
+    button.el-button{
+      padding: 0.3em;
+      margin-right: 0.5em;
+      color: rgba(63, 63, 63, 0.7);
+    }
+
+  }
+
+  @media only screen and (min-width: 1900px){
+    body .intro, body .navBar, body .footer{
+      max-width: 35vw;
+    }
+
+    h1, h2, header{
+      font-size: 4em;
+      line-height: 1em;
+    }
+
+    p, table, span{
+      line-height: unset;
+      font-size: 1.5rem;
+    }
+
   }
 </style>
